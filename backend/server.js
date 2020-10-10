174a0b185c29cf9f70b28a8611df2e1d0125d79f
chjenthankh <chjenthankh@gmail.com>
2020-10-11 05:24:19 +0700

    Try n

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,8 @@ app.get('/api/config/paypal', (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
 
+const port = process.env.PORT || 5000;
 
-app.listen(config.PORT, () => {
-  console.log('Server started at http://localhost:5000');
+server.listen(port, () => {
+    console.log("App is running on port " + port);
 });
